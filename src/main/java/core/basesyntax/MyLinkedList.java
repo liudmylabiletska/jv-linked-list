49b6d7675aeb811d7edfae1f4e88bd9f7737b4c6
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-07-24 22:51:12 +0200

    modific MyLinkedList.java

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,9 +3,7 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
-    private Node<T> head;
-    private Node<T> tail;
-    private int size;
+
 
     @Override
     public void add(T value) {
@@ -153,9 +151,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     }
 
     private static class Node<T> {
-        private T value;
+        private Node<T> head;
+        private Node<T> tail;
         private Node<T> prev;
         private Node<T> next;
+        private int size;
+        private T value;
+
 
         public Node(T value) {
             this.value = value;
